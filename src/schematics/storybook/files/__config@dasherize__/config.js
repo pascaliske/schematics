e2f22b5ed9cba36ae9d9a333281764e71d11b41a
Pascal Iske <info@pascal-iske.de>
2019-03-07 00:49:37 +0100

    fix(storybook): update setup for storybook v5

--- src/schematics/storybook/files/__config@dasherize__/config.js
+++ src/schematics/storybook/files/__config@dasherize__/config.js
@@ -1,25 +1,27 @@
-import { configure, addDecorator, moduleMetadata } from '@storybook/angular'
-import { themes } from '@storybook/components'
-import { withNotes } from '@storybook/addon-notes'
-import { withOptions } from '@storybook/addon-options'
+import { configure, addDecorator, addParameters, moduleMetadata } from '@storybook/angular'
+import { withKnobs } from '@storybook/addon-knobs'
+import { create } from '@storybook/theming'
 import { repository } from '../package.json'
 
 const load = require.context('../projects', true, /.stories.ts$/)
 
-addDecorator(withNotes)
-addDecorator(
-    withOptions({
-        name: '<%= name %>',
-        url: repository.url.replace(/.git\/?$/, ''),
-        theme: themes.<%= theme %>,
-        hierarchySeparator: /\//,
-        sortStoriesByKind: true,
-    }),
-)
+addDecorator(withKnobs)
 addDecorator(
     moduleMetadata({
         imports: [],
     }),
 )
+addParameters({
+    options: {
+        theme: create({
+            base: '<%= theme %>',
+            brandTitle: '<%= name %>',
+            brandUrl: repository.url.replace(/.git\/?$/, ''),
+        }),
+        panelPosition: 'bottom',
+        hierarchySeparator: /\//,
+        sortStoriesByKind: true,
+    },
+})
 
 configure(() => load.keys().forEach(file => load(file)), module)
